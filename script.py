b150c17db40a39b66a8feeb8a51bb383c344aed3
nikhilkoditala <nikhilkodit1@gmail.com>
2020-02-17 21:52:39 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,9 +5,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import time
 
-# gsu, gtech, oregon, uiuc,uic, purdue, cornell
-pages = ['https://www.thegradcafe.com/survey/index.php?q=georgia+state+university*+computer+science*&t=n&o=','https://www.thegradcafe.com/survey/index.php?q=georgia+institute+of+technology*+computer+science*&t=n&o=','https://www.thegradcafe.com/survey/index.php?q=oregon+state+university*+computer+science*&t=n&o=','https://www.thegradcafe.com/survey/index.php?q=university+of+illinois+urbana*+computer+science*&t=n&o=', 'https://www.thegradcafe.com/survey/index.php?q=university+of+illinois+chicago*+computer+science*&t=n&o=','https://www.thegradcafe.com/survey/index.php?q=purdue*+computer+science*&t=n&o=','https://www.thegradcafe.com/survey/index.php?q=cornell*+computer+science*&t=n&o=']
-
+# gsu, gtech, oregon
+pages = ['https://www.thegradcafe.com/survey/index.php?q=georgia+state+university*+computer+science*&t=n&o=','https://www.thegradcafe.com/survey/index.php?q=georgia+institute+of+technology*+computer+science*&t=n&o=','https://www.thegradcafe.com/survey/index.php?q=oregon+state+university*+computer+science*&t=n&o=']
 sender_email = 'sender-email-id'
 send_password = 'sender-password'
 reciever_email = 'reciever-email-id'
@@ -26,34 +25,36 @@ def crawler():
     global status
     global date
     global notes
-    
     universities = []
     program = []
     status = []
     date = []
     notes = []
-    print(lastcontent)
     for i, page in enumerate(pages):
         page = requests.get(page)
         tree = html.fromstring(page.content)
         rows = tree.xpath('//tr')
-
+        f = 1
         if(len(rows) > 1):
             for row in rows[1:]:
+                print('contents:',lastcontent[i], row.text_content())
                 if(lastcontent[i] == row.text_content()):
+                    lastcont = lastcontent[i]
                     print('yo')
-                    continue
+                    break
                 universities.append(row[0].text_content())
                 program.append(row[1].text_content())
                 status.append(row[2].text_content())
                 date.append(row[4].text_content())
                 if(len(row) == 6):
                     notes.append(row[5].text_content())
-                
-                lastcontent[i] = row.text_content()
-    
-    print(universities, program, status, date, notes)
+                if(f):
+                    lastcont = row.text_content()
+                    f = 0
+            lastcontent[i] = lastcont
 
+    print('uinivs:',universities, program, status, date, notes)
+    print('**********')
 
 def send_mail():
     print('sending email')
@@ -69,7 +70,7 @@ def send_mail():
     body = 'There are status updates in the following universities: \n'
 
     for i,_ in enumerate(universities):
-        body += universities[i] + '  ' + program[i] + '  ' + status[i] + '  ' + date[i] + '  ' + notes[i] + '\n'
+        body += universities[i] + '  ' + program[i] + '  ' + status[i] + '  ' + date[i] + '  ' + notes[i] + '\n \n'
 
     part1 = MIMEText(body, 'plain')
     msg.attach(part1)
@@ -81,6 +82,7 @@ def send_mail():
 
 while(1):
     crawler()
+    print('universities: ',universities)
     if(len(universities) > 0):
         send_mail()
     time.sleep(60*60)
